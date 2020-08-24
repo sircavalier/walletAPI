0414da7563a9d5783035ce78758b0050201d9d6e
Anderson Humberto Meneghesso <meneander@gmail.com>
2020-08-23 23:28:59 -0300

    Finalizado repositório do usuário

--- src/test/java/com/wallet/WalletApplicationTests.java
+++ src/test/java/com/wallet/WalletApplicationTests.java
@@ -1,6 +1,8 @@
 package com.wallet;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -9,10 +11,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
-class WalletApplicationTests {
+public class WalletApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertEquals(1,1);
 	}
 
 }
