4195caae534adfd8cada1581b890a100113d4a4a
Anderson Humberto Meneghesso <meneander@gmail.com>
2020-08-26 09:54:04 -0300

    Ajustes nos testes do UserController

--- src/main/java/com/wallet/controller/UserController.java
+++ src/main/java/com/wallet/controller/UserController.java
@@ -29,6 +29,11 @@ public class UserController {
 		
 		Response<UserDTO> response = new Response<UserDTO>();
 		
+		if (result.hasErrors()) {
+			result.getAllErrors().forEach(e -> response.getErrors().add(e.getDefaultMessage()));
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+		}
+		
 		User user = service.save(this.convertDtoToEntity(dto));
 		
 		response.setData(this.convertEntityToDto(user));
@@ -38,6 +43,7 @@ public class UserController {
 	
 	private User convertDtoToEntity(UserDTO dto) {
 		User u = new User();
+		u.setId(dto.getId());
 		u.setEmail(dto.getEmail());
 		u.setName(dto.getName());
 		u.setPassword(dto.getPassword());
@@ -46,7 +52,8 @@ public class UserController {
 	}
 	
 	private UserDTO convertEntityToDto(User u) {
-		UserDTO dto = new UserDTO();		
+		UserDTO dto = new UserDTO();
+		dto.setId(u.getId());
 		dto.setEmail(u.getEmail());
 		dto.setName(u.getName());
 		dto.setPassword(u.getPassword());
--- src/main/java/com/wallet/response/Response.java
+++ src/main/java/com/wallet/response/Response.java
@@ -1,5 +1,6 @@
 package com.wallet.response;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import lombok.Getter;
@@ -12,5 +13,12 @@ import lombok.Setter;
 public class Response<T> {
 
 	private T data;
-	private List<String> errors;	
+	private List<String> errors;
+	
+	public List<String> getErrors() {
+		if (this.errors == null) {
+			this.errors = new ArrayList<String>();
+		}
+		return errors;
+	}
 }
