03e7abfc28764fc81c307f2334e353d9f55f5bbe
Anderson Humberto Meneghesso <meneander@gmail.com>
2020-09-07 16:18:19 -0300

    Implementação dos novos testes da WalletItemRepository.java

--- src/test/java/com/wallet/repository/WalletItemRepositoryTest.java
+++ src/test/java/com/wallet/repository/WalletItemRepositoryTest.java
@@ -1,11 +1,17 @@
 package com.wallet.repository;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.Optional;
 
+import javax.validation.ConstraintViolationException;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +21,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.wallet.entity.Wallet;
 import com.wallet.entity.WalletItem;
+import com.wallet.util.enums.TypeEnum;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,14 +29,36 @@ import com.wallet.entity.WalletItem;
 public class WalletItemRepositoryTest {
 
 	private static final Date DATE = new Date();
-	private static final String TYPE = "ENTRADA";
+	private static final TypeEnum TYPE = TypeEnum.EN;
 	private static final String DESCRIPTION = "Conta de Luz";
 	private static final BigDecimal VALUE = BigDecimal.valueOf(65);
+	private Long savedWalletItemId = null;
+	private Long savedWalletId = null;
 	
 	@Autowired
 	WalletItemRepository repository;
 	@Autowired
 	WalletRepository walletRepository;
+	
+	@Before
+	public void setUp() {
+		Wallet w = new Wallet();
+		w.setName("Carteira Teste");
+		w.setValue(BigDecimal.valueOf(250));
+		walletRepository.save(w);
+		
+		WalletItem wi = new WalletItem(1L, w, DATE, TYPE, DESCRIPTION, VALUE);
+		repository.save(wi);
+		
+		savedWalletItemId = wi.getId();
+		savedWalletId = w.getId();
+	}
+	
+	@After
+	public void tearDown() {
+		repository.deleteAll();
+		walletRepository.deleteAll();
+	}
 	
 	@Test
 	public void testSave() {
@@ -50,4 +79,39 @@ public class WalletItemRepositoryTest {
 		assertEquals(response.getWallet().getId(), w.getId());
 	}
 	
+	@Test(expected = ConstraintViolationException.class)
+	public void testSaveInvalidWalletItem() {
+		WalletItem wi = new WalletItem(null, null, DATE, null, DESCRIPTION, null);
+		repository.save(wi);
+	}
+
+	@Test
+	public void testeUpdate() {
+		Optional<WalletItem> wi = repository.findById(savedWalletItemId);
+		
+		String description = "Descrição alterada";
+		
+		WalletItem changed = wi.get();
+		changed.setDescription(description);
+		
+		repository.save(changed);
+		
+		Optional<WalletItem> newWalletItem = repository.findById(savedWalletItemId);
+		
+		assertEquals(description, newWalletItem.get().getDescription());
+	}
+	
+	@Test
+	public void deleteWalletItem() {
+		Optional<Wallet> wallet = walletRepository.findById(savedWalletId);
+		WalletItem wi = new WalletItem(null, wallet.get(), DATE, TYPE, DESCRIPTION, VALUE);
+		
+		repository.save(wi);
+		
+		repository.deleteById(wi.getId());
+		
+		Optional<WalletItem> response = repository.findById(wi.getId());
+		
+		assertFalse(response.isPresent());
+	}
 }
